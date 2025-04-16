b88a06c5d75852bb76e65e7526e96dcd30ff12e9
pooja682002 <pooja.azhi@gmail.com>
2025-04-16 12:32:09 +0530

    first commit

---
+++ src/main/java/com/example/ProjectService/ProjectServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProjectService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectServiceApplication.class, args);
+	}
+
+}
